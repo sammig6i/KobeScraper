252eec47920d6b776f3a3740dfb1ac0e9b363343
SavviSam <swghazzawi@gmail.com>
2024-03-06 15:30:15 -0700

    api

--- scraper.py
+++ scraper.py
@@ -17,16 +17,20 @@ Signature Shot Moves:
 import openpyxl
 import pandas as pd
 
+
+
+
 class VideoData:
   def __init__(self, video_id, upload_date, signature_move, timestamps, game_details, opponent, video_url, duration):
     self.video_id = video_id
     self.upload_date = upload_date
     self.signature_move = signature_move
-    self.timestamps = timestamps
+    self.timestamps = timestamps # start and end time
     self.game_details = game_details # playoffs, nba finals, or regular season
     self.opponent = opponent
     self.video_url = video_url
     self.duration = duration
+
 
 
 
