99584ef8ee39d8738237b375d7c6659ce50f6880
SavviSam <swghazzawi@gmail.com>
2024-03-08 13:02:28 -0700

    reorganize

--- scraper.py
+++ scraper.py
@@ -15,49 +15,51 @@ from pprint import pprint
 load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
-kobe_sig_moves = ["Fadeaway",
-                  "3 Pointer",
-                  "Dunk",
-                  "Layup",
-                  "Clutch Shot",
-                  "Turnaround Fadeaway",
-                  "Post Move",
-                  "Crossover"
-                  ]
-
 class VideoData:
-  def __init__(self, video_id, upload_date, signature_move, timestamps, game_details, opponent, video_url, duration):
+  def __init__(self, video_id, upload_date, video_url, duration):
     self.video_id = video_id
     self.upload_date = upload_date
-    self.signature_move = signature_move # to be labelled
-    self.timestamps = timestamps # start and end time, to be labelled
-    self.game_details = game_details # playoffs, nba finals, or regular season, to be labelled
-    self.opponent = opponent # opposing team, to be labelled
     self.video_url = video_url # https://www.youtube.com/watch?v={videoID}
-    self.duration = duration # duration of clip, to be labelled
+    # self.signature_move = signature_move # to be labelled
+    # self.timestamps = timestamps # start and end time, to be labelled
+    # self.game_details = game_details # playoffs, nba finals, or regular season, to be labelled
+    # self.opponent = opponent # opposing team, to be labelled
+    # self.duration = duration # duration of clip, to be labelled
 
-def get_video(request):
-    api_service_name = "youtube"
-    api_version = "v3"
-    # Get credentials and create an API client
+plaery_name = "Kobe Bryant"
+signature_moves = ["Fadeaway", "3 Pointer", "Dunk", "Layup", "Clutch Shot", "Turnaround Fadeaway", "Post Move", "Crossover"]
+
+def get_video(payer_name, signature_move):
+    search_query = f'{payer_name} {signature_move} highlights'
+
     youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, developerKey=API_KEY)
+        api_service_name="youtube", api_version="v3", developerKey=API_KEY)
 
     request = youtube.search().list(
         part="id,snippet",
         type="video",
-        q=request,
+        q=search_query,
         videoDefinition='any',
         maxResults=1
     )
 
+    try:
     response = request.execute()
-    # TODO: for each signature move, request videos and save to excel spreadsheet then label
     data = response["items"][0]
     video_id = data["id"]["videoId"]
     upload_date = data["snippet"]["publishTime"]
     video_url = f'https://www.youtube.com/watch?v={video_id}'
-    print(video_id, upload_date, video_url)
+
+    except:
+    
+
+    video = VideoData(video_id, upload_date, video_url)
+    
+    return video
+
+
+
+
 
 req = "Kobe Bryant Fadeaway"
 get_video(req)
