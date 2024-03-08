f8ad47c8a9fd68a8afd48484f19a7b031c13ddaa
SavviSam <swghazzawi@gmail.com>
2024-03-08 00:52:34 -0700

    data collection

--- scraper.py
+++ scraper.py
@@ -1,17 +1,7 @@
 '''
-Kobr Bryant Signature Move Dataset
+Kobe Bryant Signature Move Dataset
 compile list of Kobe Bryant signature moves
 find videos for each signature move
-
-Signature Shot Moves:
-- Fadeaway
-- 3 Pointer
-- Dunk
-- Layup
-- Clutch Shot
-- Turnaround Fadeaway
-- Post Move
-- Crossover
 '''
 import openpyxl
 import pandas as pd
@@ -39,31 +29,38 @@ class VideoData:
   def __init__(self, video_id, upload_date, signature_move, timestamps, game_details, opponent, video_url, duration):
     self.video_id = video_id
     self.upload_date = upload_date
-    self.signature_move = signature_move
-    self.timestamps = timestamps # start and end time
-    self.game_details = game_details # playoffs, nba finals, or regular season
-    self.opponent = opponent # opposing team
+    self.signature_move = signature_move # to be labelled
+    self.timestamps = timestamps # start and end time, to be labelled
+    self.game_details = game_details # playoffs, nba finals, or regular season, to be labelled
+    self.opponent = opponent # opposing team, to be labelled
     self.video_url = video_url # https://www.youtube.com/watch?v={videoID}
-    self.duration = duration # duration of clip 
+    self.duration = duration # duration of clip, to be labelled
 
-  def get_video(self, request):
-      api_service_name = "youtube"
-      api_version = "v3"
-      # Get credentials and create an API client
-      youtube = googleapiclient.discovery.build(
-          api_service_name, api_version, developerKey=API_KEY)
+def get_video(request):
+    api_service_name = "youtube"
+    api_version = "v3"
+    # Get credentials and create an API client
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=API_KEY)
 
-      request = youtube.search().list(
-          part="id,snippet",
-          type="video",
-          q=request,
-          videoDefinition='any',
-          maxResults=1
-      )
-      response = request.execute()
+    request = youtube.search().list(
+        part="id,snippet",
+        type="video",
+        q=request,
+        videoDefinition='any',
+        maxResults=1
+    )
 
-      return response
+    response = request.execute()
+    # TODO: for each signature move, request videos and save to excel spreadsheet then label
+    data = response["items"][0]
+    video_id = data["id"]["videoId"]
+    upload_date = data["snippet"]["publishTime"]
+    video_url = f'https://www.youtube.com/watch?v={video_id}'
+    print(video_id, upload_date, video_url)
 
+req = "Kobe Bryant Fadeaway"
+get_video(req)
 
 
 
