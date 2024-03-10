1ef0749e0fc816b3596a7319414cf3655bd8ed81
SavviSam <swghazzawi@gmail.com>
2024-03-09 23:27:49 -0700

    complete get_video function

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ Kobe Bryant Signature Move Dataset
 compile list of Kobe Bryant signature moves
 find videos for each signature move
 '''
+from sys import api_version
 import openpyxl
 import pandas as pd
 import os
@@ -16,7 +17,7 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 
 class VideoData:
-  def __init__(self, video_id, upload_date, video_url, duration):
+  def __init__(self, video_id, upload_date, video_url):
     self.video_id = video_id
     self.upload_date = upload_date
     self.video_url = video_url # https://www.youtube.com/watch?v={videoID}
@@ -32,8 +33,10 @@ signature_moves = ["Fadeaway", "3 Pointer", "Dunk", "Layup", "Clutch Shot", "Tur
 def get_video(payer_name, signature_move):
     search_query = f'{payer_name} {signature_move} highlights'
 
+    api_service_name = "youtube"
+    api_version = "v3"
     youtube = googleapiclient.discovery.build(
-        api_service_name="youtube", api_version="v3", developerKey=API_KEY)
+        api_service_name, api_version, developerKey=API_KEY)
 
     request = youtube.search().list(
         part="id,snippet",
@@ -44,25 +47,26 @@ def get_video(payer_name, signature_move):
     )
 
     try:
-    response = request.execute()
-    data = response["items"][0]
-    video_id = data["id"]["videoId"]
-    upload_date = data["snippet"]["publishTime"]
-    video_url = f'https://www.youtube.com/watch?v={video_id}'
-
-    except:
-    
+        response = request.execute()
+        if not response:
+            print("No response received.")
+        else:
+            data = response["items"][0]
+            video_id = data["id"]["videoId"]
+            upload_date = data["snippet"]["publishTime"]
+            video_url = f'https://www.youtube.com/watch?v={video_id}'  
+    except Exception as err:
+        print(f"An error ocurred: {err}")
 
     video = VideoData(video_id, upload_date, video_url)
-    
     return video
 
+# TODO save video data to spreadsheet
+# def save_video():
+   
 
-
-
-
-req = "Kobe Bryant Fadeaway"
-get_video(req)
+video_data = get_video("Kobe Bryant", "Fadeaway")
+print(video_data)
 
 
 
